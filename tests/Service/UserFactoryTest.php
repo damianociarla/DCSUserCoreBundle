f2d314221cbec8ea57bcdf986483e7c25d190e8c
Damiano Ciarla <damiano.ciarla@gmail.com>
2016-11-08 15:10:22 +0100

    Increases accuracy of the test

--- tests/Service/UserFactoryTest.php
+++ tests/Service/UserFactoryTest.php
@@ -2,13 +2,24 @@
 
 namespace DCS\User\CoreBundle\Tests\Service;
 
+use DCS\User\CoreBundle\DCSUserCoreEvents;
+use DCS\User\CoreBundle\Event\UserEvent;
+use DCS\User\CoreBundle\Model\UserInterface;
 use DCS\User\CoreBundle\Service\UserFactory;
+use DCS\User\CoreBundle\Tests\TestUser;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class UserFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testCreate()
     {
-        $userFactory = new UserFactory('DCS\User\CoreBundle\Tests\TestUser');
-        $this->assertInstanceOf('DCS\User\CoreBundle\Model\UserInterface', $userFactory->create());
+        $dispatcher = $this->getMockBuilder(EventDispatcherInterface::class)->getMock();
+        $dispatcher
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with(DCSUserCoreEvents::USER_CREATED, $this->isInstanceOf(UserEvent::class));
+
+        $userFactory = new UserFactory(TestUser::class, $dispatcher);
+        $this->assertInstanceOf(UserInterface::class, $userFactory->create());
     }
 }
